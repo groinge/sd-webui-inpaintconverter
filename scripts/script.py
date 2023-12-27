80086834a9780f479ad5acf131f47737edc33a95
Based-Joar <124379871+Based-Joar@users.noreply.github.com>
2023-12-28 00:34:41 +0100

    Automatically convert model for inpainting

--- scripts/script.py
+++ scripts/script.py
@@ -2,19 +2,23 @@ import gradio as gr
 from pathlib import Path
 from modules import script_callbacks,scripts, devices, sd_hijack
 from modules.sd_models import *
+from modules.sd_models_config import find_checkpoint_config_near_filename
 from modules.timer import Timer
 from modules.modelloader import load_file_from_url
 from tqdm import tqdm
 from copy import deepcopy
+import json
 import gc
 import safetensors.torch
 import os
 import torch
 
-extension_name = 'Reload model as inpaint'
-
+extension_name = 'Reload-as-inpaint'
 EXTENSION_ROOT = scripts.basedir()
 ext2abs = lambda x: os.path.join(EXTENSION_ROOT,x)
+
+with open(ext2abs('config.json'), 'r') as f:
+    config = json.load(f)
 
 ARCHITECTURES = {
     'V1-inpaint': {
@@ -34,30 +38,45 @@ class script(scripts.Script):
             return extension_name
 
         def show(self,is_img2img):
-            if is_img2img:
-                return scripts.AlwaysVisible
+            return scripts.AlwaysVisible
         
         def ui(self,is_img2img):
-            with gr.Accordion(label=extension_name,open=False):
-                with gr.Row():
-                    button = gr.Button(value = 'Convert',scale=3,variant='primary')
-                    use_cuda = gr.Checkbox(value = True, label="Use cuda for merging")
-                with gr.Row():
-                    display = gr.Textbox(label="",value="",interactive=False,max_lines=1)
-                
-                button.click(fn=event,inputs=use_cuda,outputs=display)
+            if is_img2img:
+                with gr.Accordion(label=extension_name,open=False):
+                    with gr.Row():
+                        sets = gr.Checkboxgroup(value = config['sets'],choices=["Auto convert","Use cuda for merging"])
+                        button = gr.Button(value = 'Convert',variant='primary')
+                    with gr.Row():
+                        display = gr.Textbox(label="",value="",interactive=False,max_lines=1)
+                    
+                    button.click(fn=self.event,inputs=sets,outputs=display)
+                    sets.change(fn=sets_config,inputs=sets)
+                return [sets]
 
-def event(use_cuda):
-    message = convert(use_cuda)
-    print(extension_name+':  '+message)
-    return message
+        def setup(self,p,sets):
+            if "Auto convert" in sets:
+                if p.image_mask:
+                    self.event(sets)
+                elif model_data.sd_model and model_data.sd_model.sd_checkpoint_info.name.startswith('temp-inpainting-'):
+                    gr.Info(extension_name+':  Unloading inpainting model...')
+                    load_model(checkpoint_info=self.checkpointinfo)
 
-def convert(use_cuda):
-    device = 'cuda' if use_cuda else 'cpu'
+        def event(self,use_cuda):
+            message,self.checkpointinfo = convert(use_cuda)
+            print(extension_name+':  '+message)
+            return extension_name+':  '+message
 
-    if not model_data.sd_model: return "No checkpoint loaded."
+def sets_config(sets):
+    config['sets'] = sets
+    with open(ext2abs('config.json'), 'w') as f:
+        json.dump(config,f)
+
+def convert(sets):
+    device = 'cuda' if 'Use cuda for merging' in sets else 'cpu'
+
+    if not model_data.sd_model: return "No checkpoint loaded.",None
     sd_0, checkpoint_info, arch, message = grab_active_model_sd(device)
-    if not sd_0: sd_hijack.model_hijack.hijack(model_data.sd_model); return message
+    if not sd_0: sd_hijack.model_hijack.hijack(model_data.sd_model); return message,checkpoint_info
     fake_cp_info = deepcopy(checkpoint_info)
 
     sd_1 = load_inpaint_statedict(arch, device)
@@ -75,7 +94,7 @@ def convert(use_cuda):
         else:
             sd_0[k0] = sd_1[k1] + sd_0[k0]
 
-    fake_cp_info.name = f"temp-{checkpoint_info.name}-inpainting"
+    fake_cp_info.name = f"temp-inpainting-{checkpoint_info.name}"
 
     del sd_1
     load_model(checkpoint_info=fake_cp_info, already_loaded_state_dict=sd_0)
@@ -83,7 +102,7 @@ def convert(use_cuda):
 
     gc.collect()
     devices.torch_gc()
-    return 'Successfully converted model.'
+    return 'Successfully converted model.',checkpoint_info
 
 def load_inpaint_statedict(arch,device):
     if os.path.isfile(ext2abs(arch['filename'])):
@@ -92,33 +111,38 @@ def load_inpaint_statedict(arch,device):
         gr.Info('Downloading model (1.7 GB)...')
         file_path = load_file_from_url(arch['url'],model_dir=ext2abs('models'))
         return safetensors.torch.load_file(file_path, device=device)
-        
 
 def grab_active_model_sd(device):
     checkpoint_info = model_data.sd_model.sd_checkpoint_info
+    state_dict = model_data.sd_model.state_dict()
+
+    for info in ARCHITECTURES.values():
+        elname, idindex, iddim = info['id']
+        elem = state_dict.get(elname)
+        if list(elem) and list(elem.shape)[idindex] == iddim:
+            elname, idindex, iddim = info['type_id']
+            elem = state_dict.get(elname)
+            if list(elem) and list(elem.shape)[idindex] == iddim:
+                arch = info
+                break
+            else:
+                del state_dict; return None,None,None,'Loaded checkpoint is already an inpainting or pix-to-pix model'
+    else:
+        del state_dict; return None,None,None,'Unsupported model architecture, this extension only supports V1.5 checkpoints'
+    del state_dict
+    
+    gr.Info(extension_name+':  Converting model to inpaint...')
 
     model_data.sd_model.to(torch.device(device))
     sd_hijack.model_hijack.undo_hijack(model_data.sd_model)
 
     sd_0 = deepcopy(model_data.sd_model.state_dict())
 
-    for info in ARCHITECTURES.values():
-        elname, idindex, iddim = info['id']
-        elem = sd_0.get(elname)
-        if list(elem) and list(elem.shape)[idindex] == iddim:
-            elname, idindex, iddim = info['type_id']
-            elem = sd_0.get(elname)
-            if list(elem) and list(elem.shape)[idindex] == iddim:
-                arch = info
-                break
-            else:
-                del sd_0; return None,None,None,'Loaded checkpoint is already an inpainting or pix-to-pix model'
-    else:
-        del sd_0; return None,None,None,'Unsupported model architecture, this extension only supports V1.5 checkpoints'
-    
+    unload_model_weights(model_data.sd_model)
     model_data.sd_model = None
     gc.collect()
     devices.torch_gc()
+
     return sd_0,checkpoint_info,arch,None
 
     
